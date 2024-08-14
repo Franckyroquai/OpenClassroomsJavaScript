fb5d4b6556ba97536a9de9352d95a842837acc55
Franckyroquai <franckyroquai@gmail.com>
2024-08-14 11:56:02 +0200

    exercice boucle 2

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-// Déclaration du tableau contenant la liste des mots proposés à l'utilisateur
 const listeMots = ['Cachalot', 'Pétunia', 'Serviette']
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
+
 let score = 0
 
-for (let i = 0; i < listeMots.length; i++) {
-    let motUtilisateur = prompt("Entez le mot : " + listeMots[i])
-    if (motUtilisateur === listeMots[i]) {
-        score++
-    }
+let choix = prompt("Veuillez choisir la liste mots ou phrases")
+while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("Veuillez choisir la liste mots ou phrases")
 }
 
-// Affichage du score de l'utilisateur
-console.log("Votre score est de " + score + " sur 3")
+if (choix === "mots") {
+    for (let i = 0; i < listeMots.length; i++) {
+        let motUtilisateur = prompt("Entez le mot : " + listeMots[i])
+        if (motUtilisateur === listeMots[i]) {
+            score++
+        }
+    }
+    console.log("Votre score est de " + score + " sur " + listeMots.length)
+} else {
+    for (let i = 0; i < listePhrases.length; i++) {
+        let motUtilisateur = prompt("Entez la phrase : " + listePhrases[i])
+        if (motUtilisateur === listePhrases[i]) {
+            score++
+        }
+    }
+    console.log("Votre score est de " + score + " sur " + listePhrases.length)
+}
