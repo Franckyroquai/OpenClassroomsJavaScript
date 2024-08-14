55c2f34b10407d26adf18de84e8db789e03f9c35
Franckyroquai <franckyroquai@gmail.com>
2024-08-14 11:41:30 +0200

    exercice conditions

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-let listeMots = ["Cachalot", "Pétunia", "Serviette"]
-
+// Déclaration du tableau contenant la liste des mots proposés à l'utilisateur
+const listeMots = ['Cachalot', 'Pétunia', 'Serviette']
 let score = 0
 
-let motUtilisateur = prompt("Entrez le mot : " + listeMots[0])
+// Déclaration de la variable contenant le mot saisi par l'utilisateur
+// Le prompt sert à afficher une popup demandant à l'utilisateur de saisir un mot
+let motUtilisateur = prompt('Entrez le mot : ' + listeMots[0])
 
+// Vérification du mot saisi par l'utilisateur : on vérifie qu'il correspond au premier mot du tableau "listeMots"
 if (motUtilisateur === listeMots[0]) {
-    score ++
+    score++
 }
 
-motUtilisateur = prompt("Entrez le mot : " + listeMots[1])
-
+// Idem avec le second mot du tableau
+// Notez l'absence du "let" devant la déclaration de la variable "motUtilisateur", cela permet de réutiliser la variable déclarée précédemment
+motUtilisateur = prompt('Entrez le mot : ' + listeMots[1])
 if (motUtilisateur === listeMots[1]) {
-    score ++
+    score++
 }
 
-motUtilisateur = prompt("Entrez le mot : " + listeMots[2])
-
+// Idem avec le troisième mot du tableau
+motUtilisateur = prompt('Entrez le mot : ' + listeMots[2])
 if (motUtilisateur === listeMots[2]) {
-    score ++
+    score++
 }
 
-console.log(score)
+// Affichage du score de l'utilisateur
+console.log("Votre score est de " + score + " sur 3")
